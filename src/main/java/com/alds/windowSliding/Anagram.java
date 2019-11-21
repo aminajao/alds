beee984433b796ecd1c413dd4f9b6ef062958cb2
Rohit Singh <rohit.singh@blackrock.com>
2019-11-21 16:25:03 +0800

    modified the structure

---
+++ src/main/java/com/alds/windowSliding/Anagram.java
@@ -0,0 +1,22 @@
+/*
+* Copyright (C) 2019 BlackRock.
+*
+* Created on Nov 21, 2019
+*
+* Last edited by: $Author: $
+*             on: $Date: $
+*       Filename: $Id:  $
+*       Revision: $Revision: $
+*/
+package com.alds.windowSliding;
+
+/**
+ * @author rohsingh
+ * 
+ * LISTEN - SILENT
+ * TRIANGLE - INTEGRAL
+ *
+ */
+public class Anagram {
+
+}
---
+++ src/main/java/com/alds/windowSliding/LongestSubstringWithDistinctChars.java
@@ -0,0 +1,32 @@
+/*
+* Copyright (C) 2019 BlackRock.
+*
+* Created on Nov 21, 2019
+*
+* Last edited by: $Author: $
+*             on: $Date: $
+*       Filename: $Id:  $
+*       Revision: $Revision: $
+*/
+package com.alds.windowSliding;
+
+/**
+ * @author rohsingh
+ * 
+ *  Find the longest substring with ‘K’ distinct characters 
+   
+   "aabbcc", k = 1
+    Max substring can be any one from {"aa" , "bb" , "cc"}.
+    
+    "aabbcc", k = 2
+    Max substring can be any one from {"aabb" , "bbcc"}.
+
+    "aabbcc", k = 3
+    There are substrings with exactly 3 unique characters
+    {"aabbcc" , "abbcc" , "aabbc" , "abbc" }
+    Max is "aabbcc" with length 6.
+ *
+ */
+public class LongestSubstringWithDistinctChars {
+
+}
