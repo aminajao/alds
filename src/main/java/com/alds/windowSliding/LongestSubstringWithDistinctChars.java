7d008c1121454440b504efd2bd224a63b08bff62
rohsi <rohsi@LAPTOP-A9HEDL05>
2019-11-24 23:59:45 +0800

    added new classes

--- src/main/java/com/alds/windowSliding/LongestSubstringWithDistinctChars.java
+++ src/main/java/com/alds/windowSliding/LongestSubstringWithDistinctChars.java
@@ -13,7 +13,7 @@ package com.alds.windowSliding;
 /**
  * @author rohsingh
  * 
- *  Find the longest substring with K distinct characters 
+ *  Find the longest substring with K unique characters 
    
    "aabbcc", k = 1
     Max substring can be any one from {"aa" , "bb" , "cc"}.
@@ -30,6 +30,20 @@ package com.alds.windowSliding;
 public class LongestSubstringWithDistinctChars {
 	
 	public String findB(String str, int k) {
+	
+		int n = str.length();
+		String res;
+		
+		int i = 1;
+		String start = str.substring(k-1);
+		
+		while(i < n) {
+			res = str.substring(i);
+			i++;
+		}
+			
+			
+			
 		return str;
 	
 	}
