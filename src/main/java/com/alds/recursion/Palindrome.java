85d89af4e8e133620314ef01dc1a541cd3b2996f
rohsi <rohsi@LAPTOP-A9HEDL05>
2019-11-27 00:14:51 +0800

    palindrome functions

---
+++ src/main/java/com/alds/recursion/Palindrome.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.alds.recursion;
+
+/**
+ * @author rohsi
+ *
+ */
+public class Palindrome {
+	
+	// iterative
+	public static boolean isNumberPalindrome(int n) {
+		
+	}
+	
+	//recursion
+	public static boolean isNumberPalindromeR(int n) {
+		
+	}
+	
+	// recursion - reverse string approach
+	public static boolean isStringPalindrome(String s) {
+		
+	}
+
+}
