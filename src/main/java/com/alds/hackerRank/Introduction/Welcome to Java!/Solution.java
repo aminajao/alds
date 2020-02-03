c3acefcf56a32f0854ddd1d927f8c33de300ddd4
rohsi <rohsi@LAPTOP-A9HEDL05>
2020-02-03 09:55:27 +0800

    remove unnecessary packages

--- src/main/java/com/alds/hackerRank/Introduction/Welcome to Java!/Solution.java
+++
@@ -1,9 +0,0 @@
-// Author: Rodney Shaghoulian
-// Github: github.com/RodneyShag
-
-public class Solution {
-    public static void main(String[] args) {
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
-    }
-}
