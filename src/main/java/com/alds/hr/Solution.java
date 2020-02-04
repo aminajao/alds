3e0a4aea886318a809b096f82d2ffde5e8a5799d
rohsi <rohsi@LAPTOP-A9HEDL05>
2020-02-04 11:51:34 +0800

    removed unused code

--- src/main/java/com/alds/hr/Solution.java
+++ src/main/java/com/alds/hr/Solution.java
@@ -5,20 +5,5 @@ package com.alds.hr;
  *
  */
 public class Solution {
-    
-    id AS `Id`,
-    CASE
-        WHEN tree.id = (SELECT atree.id FROM tree atree WHERE atree.p_id IS NULL)
-          THEN 'Root'
-        WHEN tree.id IN (SELECT atree.p_id FROM tree atree)
-          THEN 'Inner'
-        ELSE 'Leaf'
-    END AS Type
-FROM
-    tree
-ORDER BY `Id`
-;
-    
-    
 
 }
