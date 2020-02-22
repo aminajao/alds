578f70b26d3f96dbda7fd4b2e359322354ed16ef
rohsi <rohsi@LAPTOP-A9HEDL05>
2020-02-22 18:16:34 +0800

    added code for klarna solution

--- src/main/java/com/alds/hr/Solution.java
+++ src/main/java/com/alds/hr/Solution.java
@@ -6,35 +6,5 @@ package com.alds.hr;
  */
 public class Solution {
     
-    public float openHoursRatio(TimeRange queryTimeRange, List<TimeRange> openHours) {
-  //Convert person timings to minutes
-    double personStartTime=(double)(queryTimeRange.start)*60;
-    double personEndTime=(double)(queryTimeRange.end)*60;
-
-    //Count the overlap between intervals
-    double overlap = 0.0;
-    for(TimeRange t : openHours)
-    {
-        //Convert store timings to minutes
-        double storeStartTime=(double)(t.start) * 60;
-        double storeEndTime=(double)(t.end) * 60;
-        
-        //no overlap 
-        if(storeStartTime > personEndTime || personStartTime > storeEndTime)
-            continue;
-
-        //Get the latest start    
-        double overlapStart= Math.max(personStartTime,storeStartTime);
-        //Get the earliest end 
-        double overlapEnd= Math.min(personEndTime,storeEndTime);
-        //Total overlap 
-        overlap+=overlapEnd-overlapStart;
-
-    }
-    //To avoid divide by 0
-    if(personEndTime!=personStartTime)
-        return (float)(overlap/(personEndTime - personStartTime));
-    return (float)overlap;
-    }
-    
+   
 }
--- src/main/java/com/alds/hr/klarna/Klarna.java
+++ src/main/java/com/alds/hr/klarna/Klarna.java
@@ -10,9 +10,12 @@
 */
 package com.alds.hr.klarna;
 
+import java.util.List;
 import java.util.Set;
+import java.util.Stack;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * @author rohsingh
@@ -20,30 +23,53 @@ import java.util.stream.IntStream;
  * 
 1. You're given a number and you should write the multiples of 3 and 5 up to that number. Using a set is straightforward.
 
-2. You're given a String consisting of positive integers delimited by whitespace. You must get these integers and weight sort them. Sort first according to the sum of digits in each integer, if the sums are equal, the smaller one comes first. Use Java8 Comparator. comparing
+2. You're given a String consisting of positive integers delimited by whitespace. You must get these integers and weight sort them. 
+	Sort first according to the sum of digits in each integer, if the sums are equal, the smaller one comes first. Use Java8 Comparator.comparing
 
 3. Typical BraceChecker question. See if left parentheses match right parentheses. Use stack!  
  *
  */
 public class Klarna {
       
-    public static void mutipleOf3And5(int n) {
-        Set<Integer> out = 
-           IntStream.range(3, n)
-            .filter(i -> (i%3==0) || (i%5==0))
-            .boxed()
-            .collect(Collectors.toSet());
-        
-        System.out.println(out);
-    }
+	public static void mutipleOf3And5(int n) {
+		Set<Integer> out = IntStream.range(3, n).filter(i -> (i % 3 == 0) || (i % 5 == 0)).boxed()
+				.collect(Collectors.toSet());
+		System.out.println(out);
+	}
     
-    public static void weightSorting(String in) {
-        
-    }
+	public static void weightSorting(String in) {
+		List<Integer> out = Stream.of(in.split(" ")).map(Integer::valueOf).sorted((i1, i2) -> i1.compareTo(i2))
+				.collect(Collectors.toList());
+		System.out.println(out);
+	}
     
-    public static void braceChecker(String in) {
-        
-    }
+	public static boolean braceChecker(String in) {
+		Stack<Character> s = new Stack<Character>();
+		// Iterating over the entire string.
+		for (char st : in.toCharArray()) {
+			// If the input string contains an opening parenthesis,
+			// push in on to the stack.
+			if (st == '(' || st == '{' || st == '[') {
+				s.push(st);
+			} else {
+				// In the case of valid parentheses, the stack cannot be
+				// be empty if a closing parenthesis is encountered.
+				if (s.empty()) {
+					return false;
+				}
+				// If the input string contains a closing bracket,
+				// then pop the corresponding opening parenthesis if present.
+				char top = (Character) s.peek();
+				if (st == ')' && top == '(' || st == '}' && top == '{' || st == ']' && top == '[') {
+					s.pop();
+				}
+			}
+		}
+		if(!s.empty()) {
+			return false;
+		} 
+		return true;
+	}
     
     public static void main(String[] args) {
         
