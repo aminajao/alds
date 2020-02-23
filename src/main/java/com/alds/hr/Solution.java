09622107167b1b5ee49ad46a1d84e49be13848cf
Rohit Singh <rohit.singh@blackrock.com>
2020-02-23 22:45:17 +0800

    added yelp solution

--- src/main/java/com/alds/hr/Solution.java
+++ src/main/java/com/alds/hr/Solution.java
@@ -6,5 +6,21 @@ package com.alds.hr;
  */
 public class Solution {
     
-   
+    public static List<String> findReachableBusinesses(Business startingBusiness, int distance) {
+        visited.put(startingBusiness, true);
+        dfs(startingBusiness, distance, 0);
+        return answer;
+    }
+    static List<String> answer = new ArrayList<>();
+    static Map<Business, Boolean> visited = new HashMap<>();
+    public static void dfs(Business startingBusiness, int distance, int curr){
+        Map<Business, Integer> near = startingBusiness.getNearbyBusinesses();
+        for(Business b: near.keySet()){
+            if(visited.getOrDefault(b, false)==false && near.get(b) + curr <= distance){
+                visited.put(b, true);
+                answer.add(b.getName());
+                dfs(b, distance, curr + near.get(b));
+            }
+        }
+    }
 }
