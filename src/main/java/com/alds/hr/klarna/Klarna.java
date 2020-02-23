680d69bf574ee3fe1adf2075b04dde67aa5b34b3
Rohit Singh <rohit.singh@blackrock.com>
2020-02-23 17:50:03 +0800

    added new code for klarna

--- src/main/java/com/alds/hr/klarna/Klarna.java
+++ src/main/java/com/alds/hr/klarna/Klarna.java
@@ -150,4 +150,47 @@ public class Klarna {
         }
         return ordinalSuffix;
     }
+    
+    private Set<String> operators = new HashSet<>(Arrays.asList("/", "+","-","*"));
+    private Stack<Double> bucket = new Stack<>();
+
+    public double evaluate(String expr) {
+
+        if(expr == null || expr.isEmpty()){
+            return 0.0;
+        }
+
+        Arrays.stream(expr.split(" ")).forEach(input->{
+            if (operators.contains(input)) {
+                Double operand2 = bucket.pop();
+                Double operand1 = bucket.pop();
+                Double result = applyOperator(input,operand1,operand2);
+                bucket.push(result);
+            }else{
+                bucket.push(Double.parseDouble(input));
+            }
+
+        });
+        return bucket.pop();
+    }
+
+    private double applyOperator(String operator, Double operand1, Double operand2){
+        Double result = null;
+        switch (operator){
+            case "/":
+                result = operand1 / operand2;
+                break;
+            case "*":
+                result = operand1 * operand2;
+                break;
+            case "-":
+                result = operand1 - operand2;
+                break;
+            case "+":
+                result = operand1 + operand2;
+                break;
+        }
+
+        return result;
+    }
 }
