214fe966f09fd756849eb7ac0d49bd3560e0d116
rohsi <rohsi@LAPTOP-A9HEDL05>
2020-03-09 09:51:24 +0800

    updated

--- src/main/java/com/alds/hr/Solution.java
+++ src/main/java/com/alds/hr/Solution.java
@@ -6,19 +6,4 @@ package com.alds.hr;
  */
 public class Solution {
     
-   // https://github.com/thiaguimcavalcanti/hacker-rank-restapi-with-spring-boot.git
-    /**
-    <dependency>
-    <groupId>org.hibernate</groupId>
-    <artifactId>hibernate-entitymanager</artifactId>
-    <version>4.3.7.Final</version>
-</dependency>
-
-/**
- * 
-SELECT "Donor" AS Type Gender,BG, COUNT(BG) CountValue FROM Donor GROUP BY Gender,BG
-UNION ALL
-SELECT "ACCEPTOR" AS Type Gender,BG, COUNT(BG) CountValue FROM ACCEPTOR GROUP BY Gender,BG
- */
-	
 }
