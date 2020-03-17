375830b91eb565ca2da9cc4a4bfae98974d953bb
rohsi <rohsi@LAPTOP-A9HEDL05>
2020-03-17 17:22:08 +0800

    Final code for elevator system

--- src/main/java/com/alds/design/elevator/ElevatorMain.java
+++ src/main/java/com/alds/design/elevator/ElevatorMain.java
@@ -1,6 +1,9 @@
 package com.alds.design.elevator;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import com.alds.design.elevator.controller.ElevatorController;
+import com.alds.design.elevator.service.ElevatorService;
 
 /**
  * @author rohsingh
@@ -9,6 +12,17 @@ import com.alds.design.elevator.controller.ElevatorController;
 public class ElevatorMain {
 
 	public static void main(String[] args) {
-		new ElevatorController(2);
+		ElevatorController controller = new ElevatorController(new ElevatorService(2));
+		new Thread(() ->  {
+			while(true) {
+				controller.sendFloorRequest(ThreadLocalRandom.current().nextInt(10));
+			}
+		}).start();
+		
+		new Thread(() ->  {
+			while(true) {
+				controller.consumeFloorRequest();
+			}
+		}).start();
 	}
 }
--- src/main/java/com/alds/design/elevator/model/Elevator.java
+++ src/main/java/com/alds/design/elevator/model/Elevator.java
@@ -13,42 +13,98 @@ public class Elevator {
 	int currentFloor;
 	ElevatorStatus status;
 	ElevatorDirection direction;
-	ConcurrentSkipListSet<ElevatorCommand> upCommands;
-	ConcurrentSkipListSet<ElevatorCommand> downCommands;
+	ConcurrentSkipListSet<Integer> upFloorRequests;
+	ConcurrentSkipListSet<Integer> downFloorRequests;
 
 	public Elevator() {
 		this(1);
 	}
 
 	public Elevator(int elevatorId) {
-		this(elevatorId, ElevatorStatus.IDLE);
+		this(elevatorId, 0);
 	}
 
-	public Elevator(int elevatorId, ElevatorStatus status) {
+	public Elevator(int elevatorId, int currentFloor) {
 		this.elevatorId = elevatorId;
-		this.status = status;
-		this.upCommands = new ConcurrentSkipListSet<>();
-		this.downCommands = new ConcurrentSkipListSet<>(new Comparator<ElevatorCommand>() {
-			public int compare(ElevatorCommand o1, ElevatorCommand o2) {
+		this.currentFloor = currentFloor;
+		this.direction = ElevatorDirection.NONE;
+		this.status = ElevatorStatus.IDLE;
+		this.upFloorRequests = new ConcurrentSkipListSet<>();
+		this.downFloorRequests = new ConcurrentSkipListSet<>(new Comparator<Integer>() {
+			public int compare(Integer o1, Integer o2) {
 				return o2.compareTo(o1);
 			}
 		});
+	}
+
+	public boolean updateStatus(ElevatorStatus elevatorStatus){
+		if(getTotalFloorRequests() > 0){
+			return false;
+		}
+		status = elevatorStatus;
+		return true;
+	}
+
+	public int getNextFloorDestination(){
+        if(direction == ElevatorDirection.DOWN){
+            return downFloorRequests.first();
+        }else if(direction == ElevatorDirection.UP){
+            return upFloorRequests.first();
+        }else{
+            return 0;
+        }
+    }
+	
+	public void addNewFloorRequest(int newFloor) {
+		if (newFloor > currentFloor) {
+			upFloorRequests.add(newFloor);
+		} else {
+			downFloorRequests.add(newFloor);
+		}
+		System.out.println("Added Request For Elevator : "+elevatorId+ " Up : "+upFloorRequests.size()+" Down : "+downFloorRequests.size());
+	}
+
+	public boolean moveAndCheckIfServed() {
+		if (direction== ElevatorDirection.NONE) {
+			if (upFloorRequests.size() > 0 && downFloorRequests.size() > 0) {
+				if (Math.abs(currentFloor - upFloorRequests.first()) < Math.abs(currentFloor - downFloorRequests.first())) {
+					direction = ElevatorDirection.UP;
+				} else {
+					direction = ElevatorDirection.DOWN;
+				}
+			} else if (upFloorRequests.size() > 0) {
+				direction = ElevatorDirection.UP;
+			} else if (downFloorRequests.size() > 0) {
+				direction = ElevatorDirection.DOWN;
+			}
+		}
+
+		if(direction == ElevatorDirection.UP){
+			if(upFloorRequests.first() == currentFloor){
+				upFloorRequests.pollFirst();
+				return true;
+			}else {
+				currentFloor++;
+			}
+		}else if(direction == ElevatorDirection.DOWN){
+			if(downFloorRequests.first() == currentFloor){
+				downFloorRequests.pollFirst();
+				return true;
+			} else {
+				currentFloor--;
+			}
+		}else{
+			//Do Nothing. Elevator is not moving.
+		}
+		return false;
 	}
 
 	public ElevatorDirection getDirection() {
 		return direction;
 	}
 
-	public void setDirection(ElevatorDirection direction) {
-		this.direction = direction;
-	}
-
 	public ElevatorStatus getStatus() {
 		return status;
-	}
-
-	public void setStatus(ElevatorStatus status) {
-		this.status = status;
 	}
 
 	public int getElevatorId() {
@@ -59,17 +115,8 @@ public class Elevator {
 		return currentFloor;
 	}
 
-	public void setCurrentFloor(int currentFloor) {
-		this.currentFloor = currentFloor;
+	public int getTotalFloorRequests() {
+		return upFloorRequests.size() + downFloorRequests.size();
 	}
-
-	public ConcurrentSkipListSet<ElevatorCommand> getUpCommands() {
-		return upCommands;
-	}
-
-	public ConcurrentSkipListSet<ElevatorCommand> getDownCommands() {
-		return downCommands;
-	}
-
 	
 }
